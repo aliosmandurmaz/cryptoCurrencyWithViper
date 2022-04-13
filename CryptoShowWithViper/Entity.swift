b5c79d53634bbedb9cf148ec2ec40436195a30a5
aliosmandurmaz <odurmaz.34@outlook.com>
2022-04-13 17:46:03 +0300

    first commit

---
+++ CryptoShowWithViper/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  CryptoShowWithViper
+//
+//  Created by Ali Osman DURMAZ on 9.04.2022.
+//
+
+import Foundation
+
+struct Crypto: Codable {
+    let currency: String
+    let price: String
+}
---
+++ CryptoShowWithViper/Interactor.swift
@@ -0,0 +1,43 @@
+//
+//  Interactor.swift
+//  CryptoShowWithViper
+//
+//  Created by Ali Osman DURMAZ on 9.04.2022.
+//
+
+import Foundation
+
+protocol AnyInteractor {
+    var presenter: AnyPresenter? {get set}
+    
+    func downloadCryptos()
+}
+
+class CryptoInteractor: AnyInteractor {
+   
+    var presenter: AnyPresenter?
+    
+    func downloadCryptos() {
+        guard let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data , error == nil else {
+                self.presenter?.interactorDidDownloadCrypto(result: .failure(NetworkError.NetworkFailed))
+                return
+            }
+            
+            do {
+                let cryptos = try JSONDecoder().decode([Crypto].self, from: data)
+            } catch  {
+                self.presenter?.interactorDidDownloadCrypto(result: .failure(NetworkError.ParsingFailed))
+            }
+            
+        }
+        task.resume()
+    }
+    
+    
+}
